ee43e2cc91f3e68a27730f5406521a10b4c7a508
Anisha Nayaju <anishanayaju26@gmail.com>
2025-03-05 12:33:57 +0545

    food routes

--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -1,8 +1,19 @@
 import express from "express";
 import multer from "multer";
-
-
+import { addFood } from "../controllers/foodcontrollers.js";
 
 const foodRouter = express.Router();
+
+//Image Search Engine
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+const uploads = multer({ storage: storage });
+
+foodRouter.post("/add", uploads.single("image"), addFood);
 
 export default foodRouter;
