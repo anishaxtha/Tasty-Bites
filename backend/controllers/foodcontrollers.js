e37380f55587d705926d9877313d62e5c7a56ac3
Anisha Nayaju <anishanayaju26@gmail.com>
2025-03-05 12:33:44 +0545

    food added

--- backend/controllers/foodcontrollers.js
+++ backend/controllers/foodcontrollers.js
@@ -2,6 +2,35 @@ import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
 //add food item
-const addFood = async (req, res) => {};
+const addFood = async (req, res) => {
+  console.log(req.body); // Log the body to check if 'price' is there
+  if (!req.file) {
+    return res.status(400).json({
+      success: false,
+      message: "No file uploaded",
+    });
+  }
+  let image_filename = `${req.file.filename}`;
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+  try {
+    await food.save();
+    res.json({
+      success: true,
+      message: "Food item added successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Error while adding food item",
+    });
+  }
+};
 
 export { addFood };
