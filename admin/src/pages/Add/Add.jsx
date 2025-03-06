5f2fee5b76d8c6e961ca943fd9da8ca2994e225f
Anisha Nayaju <anishanayaju26@gmail.com>
2025-03-06 14:14:48 +0545

    Add section done

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,20 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { assets } from "../../assets/assets";
 import "../Add/Add.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Add = () => {
+  const url = "http://localhost:4000";
   const [image, setImage] = useState(false);
+  const [preview, setPreview] = useState(null); // New state for preview
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Salad",
+  });
+
+  const onchangeHandler = (event) => {
+    const { name, value } = event.target;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file);
+      setPreview(URL.createObjectURL(file)); // Generate preview URL
+    }
+  };
+  // just checking the data is being updated or not
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", Number(data.price));
+    formData.append("category", data.category);
+    formData.append("image", image);
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    console.log("🚀 ~ onSubmitHandler ~ response:", response.data);
+
+    if (response.data.success) {
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad",
+      });
+      setImage(false);
+      toast.success(response.data.message);
+    } else {
+      // console.log("submission failed", response.data);
+      toast.error(response.data.message);
+    }
+  };
 
   return (
     <div className="add">
-      <form className="flex-col">
+      <form onSubmit={onSubmitHandler} className="flex-col">
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
+            <img src={preview || assets.upload_area} alt="" />
           </label>
           <input
-            onChange={(e) => setImage(e.target.files[0])}
+            // onChange={(e) => setImage(e.target.files[0])}
+            onChange={handleImageChange}
             type="file"
             id="image"
             hidden
@@ -23,12 +78,20 @@ const Add = () => {
         </div>
         <div className="add-product-name flex-col">
           <p>Product Name</p>
-          <input type="text" name="name" placeholder="Type here" />
+          <input
+            onChange={onchangeHandler}
+            value={data.name}
+            type="text"
+            name="name"
+            placeholder="Type here"
+          />
         </div>
 
         <div className="add-product-description flex-col">
           <p>Product Description</p>
           <textarea
+            onChange={onchangeHandler}
+            value={data.description}
             name="description"
             rows="6"
             placeholder="Write Content here"
@@ -39,7 +102,7 @@ const Add = () => {
         <div className="add-category-price">
           <div className="add-category flex-col">
             <p>Product Category</p>
-            <select name="category">
+            <select onChange={onchangeHandler} name="category">
               <option value="Salad">Salad</option>
               <option value="Rolls">Rolls</option>
               <option value="Deserts">Deserts</option>
@@ -53,7 +116,13 @@ const Add = () => {
 
           <div className="add-price flex-col">
             <p>Product Price</p>
-            <input type="Number" name="price" placeholder="$20" />
+            <input
+              onChange={onchangeHandler}
+              value={data.price}
+              type="Number"
+              name="price"
+              placeholder="$20"
+            />
           </div>
         </div>
 
