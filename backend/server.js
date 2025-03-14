66575effcc24f22237979457e7a85173663819b6
Anisha Nayaju <anishanayaju26@gmail.com>
2025-03-14 20:35:04 +0545

    server page

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import connectDB from "./config/db.js";
 import foodRouter from "./routes/foodRoutes.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoutes.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // import { EventEmitter } from "events";
 // EventEmitter.defaultMaxListeners = 15; // Set to 0 for unlimited
@@ -27,6 +28,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working...");
