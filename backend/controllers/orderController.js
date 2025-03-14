2648b57f74d97215e2a98988e6b27ffb1f88a851
Anisha Nayaju <anishanayaju26@gmail.com>
2025-03-14 20:34:29 +0545

    order controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,9 @@
+import Stripe from "Stripe";
+import userModel from "../models/userModel";
+import orderModel from "../models/orderModel";
+
+// placing the order model for frontend
+
+const placeOrder = async (req, res) => {};
+
+export { placeOrder };
